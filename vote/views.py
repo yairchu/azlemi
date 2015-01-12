46a868e9a6daf1cceaa371a04aa6bf6c99e953c8
Yair Chuchem <yairchu@gmail.com>
2015-01-12 15:33:45 +0200

    questions marked interesting now given 90% of the time

--- vote/views.py
+++ vote/views.py
@@ -145,13 +145,12 @@ def fetch_vote(vote_id):
 
 def choose_question_set(already_asked):
     result = set()
-    r = random.random()
-    if r < 0.6:
+    if random.random() < 0.9:
         result = set(
             x.id for x in
             models.Vote.objects.filter(is_interesting = True)
             ) - already_asked
-    if not result and r < 0.8:
+    if not result and random.random() < 0.6:
         result = set(
             x.id for x in
             models.Vote.objects.filter(
