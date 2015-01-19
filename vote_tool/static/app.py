d0c98d4bb7f6671c4384d29bc728d9c84f6de21d
Yair Chuchem <yairchu@gmail.com>
2015-01-19 23:06:49 +0200

    Revert "Results for parties of 20th knesset (as yet known)"
    This reverts commit e74bdf6137977c33c3d9b8fac058ca2d10d964ff.

--- vote_tool/static/app.py
+++ vote_tool/static/app.py
@@ -7,6 +7,8 @@ from browser import ajax, document, html, timer
 
 import render_content
 
+party_of_member = dict((x['id'], x['party_id']) for x in members)
+
 class Question:
     def __init__(self, data):
         self.data = data
@@ -35,7 +37,7 @@ class Question:
         party_votes_doc.clear()
         if self.answer is None:
             return
-        render_content.question_party_votes(party_votes_doc, self.data, self.answer)
+        render_content.question_party_votes(party_votes_doc, self.data, self.answer, parties)
 
 def is_boring_question(question_data):
     for x in ['for_votes_count', 'against_votes_count']:
@@ -109,11 +111,12 @@ class Game:
             dict((q.data['id'], q.data) for q in self.questions),
             dict((q.data['id'], q.answer) for q in self.questions
                 if q.answer is not None),
+            parties,
             )
 
         document['results'].clear()
         document['results-small'].clear()
-        render_content.render_results(document['results'], document['results-small'], results)
+        render_content.render_results(document['results'], document['results-small'], results, parties)
 
 def id_from_uri(uri):
     return int(uri.rstrip('/').rsplit('/', 1)[1])
