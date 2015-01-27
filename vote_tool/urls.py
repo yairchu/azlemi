64a4694b45ac1fc24b4a60814a3c4801c5d54933
Yair Chuchem <yairchu@gmail.com>
2015-01-28 01:20:10 +0200

    google webmaster tools verification code

--- vote_tool/urls.py
+++ vote_tool/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
     url(r'^get_question/(?P<question_id>[0-9]+)/$', 'vote.views.get_specific_question'),
     url(r'^admin/', include('smuggler.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^googlec8560e89bf08df0f.html', 'vote.views.google_webmaster_tools_verification'),
     url(r'', include('feincms.urls'))
 )
