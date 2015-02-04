cb416d2ee6006fd13ecac1df64dea36fdf8137a0
Yair Chuchem <yairchu@gmail.com>
2015-02-04 14:02:33 +0200

    admin site add HasTranslationFilter

--- vote/admin.py
+++ vote/admin.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 from django.contrib import admin
 
 from modeltranslation.admin import TabbedTranslationAdmin
@@ -26,9 +27,41 @@ class VoteForm(forms.ModelForm):
         exclude = []
         widgets = vote_form_widgets()
 
+class HasTranslationFilter(admin.FieldListFilter):
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.request = request
+        super(HasTranslationFilter, self).__init__(
+            field, request, params, model, model_admin, field_path)
+        self.title = '%s Translations' % (field.verbose_name)
+
+    def expected_parameters(self):
+        for lang_key, lang_name in settings.LANGUAGES[1:]:
+            yield '%s_%s__exact' % (self.field_path, lang_key)
+
+    def choices(self, cl):
+        params = set()
+        any_chosen = False
+        for param in self.expected_parameters():
+            params.add(param)
+            is_chosen = self.request.GET.get(param) == ''
+            any_chosen = any_chosen or is_chosen
+        yield {
+            'selected': not any_chosen,
+            'display': 'Any',
+            'query_string': cl.get_query_string({}, params),
+            }
+        for lang_key, lang_name in settings.LANGUAGES[1:]:
+            param = '%s_%s__exact' % (self.field_path, lang_key)
+            is_chosen = self.request.GET.get(param) == ''
+            yield {
+                'selected': is_chosen,
+                'display': 'Lacks %s' % lang_name,
+                'query_string': cl.get_query_string({param: ''}, params),
+                }
+
 class VoteAdmin(TabbedTranslationAdmin):
     form = VoteForm
-    list_filter = ['is_interesting']
+    list_filter = ['is_interesting', ('vt_title', HasTranslationFilter)]
 
 admin.site.register(models.UserAnswer)
 admin.site.register(models.Vote, VoteAdmin)
