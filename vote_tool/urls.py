12977dcb8fe30f84ee4afc78c1b748bbf780a78c
Yair Chuchem <yairchu@gmail.com>
2015-02-05 15:28:35 +0200

    add admin page for log of recent actions from all admins

--- vote_tool/urls.py
+++ vote_tool/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
@@ -14,6 +15,9 @@ urlpatterns = patterns('',
     url(r'^get_question/$', 'vote.views.get_question'),
     url(r'^get_question/(?P<question_id>[0-9]+)/$', 'vote.views.get_specific_question'),
     url(r'^restart/$', 'vote.views.restart'),
+    url(r'^admin/recent_actions/$',
+        login_required(login_url='/admin/login/')(
+            TemplateView.as_view(template_name='admin/recent_actions.html'))),
     url(r'^admin/', include('smuggler.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
@@ -27,3 +31,24 @@ if settings.DEBUG:
 urlpatterns += patterns('',
     url(r'', include('feincms.urls'))
 )
+
+### MONKEY PATCHES FOR DJANGO BUGS:
+
+# https://github.com/django/django/pull/4060
+import django.contrib.admin.templatetags.log
+class AdminLogNode(django.contrib.admin.templatetags.log.AdminLogNode):
+    def render(self, context):
+        from django import template
+        from django.contrib.admin.models import LogEntry
+        if self.user is None:
+            entries = LogEntry.objects.all()
+        else:
+            user_id = self.user
+            if not user_id.isdigit():
+                user_id = context[self.user].pk
+            entries = LogEntry.objects.filter(user__pk=user_id)
+        context[self.varname] = entries.select_related(
+            'content_type', 'user',
+            )[:int(self.limit)]
+        return ''
+django.contrib.admin.templatetags.log.AdminLogNode = AdminLogNode
