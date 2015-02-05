7f03c29e708ac662fd183775068acb64c4f986d0
Yair Chuchem <yairchu@gmail.com>
2015-02-05 14:12:32 +0200

    set static url to go through cdn

--- vote_tool/settings.py
+++ vote_tool/settings.py
@@ -133,7 +133,7 @@ ALLOWED_HOSTS = ['*']
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' if DEBUG else 'http://cdn.azlemi.org.il/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
